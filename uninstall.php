72dd959b0bef2bd7fc2b2e50a9bccc131c974e91
Ulrik Moe <ulrik.moe@gmail.com>
2023-01-15 18:36:53 +0100

    Add missing table

--- uninstall.php
+++ uninstall.php
@@ -3,10 +3,13 @@
 defined('ABSPATH') || exit();
 defined('WP_UNINSTALL_PLUGIN') || die();
 
-// delete database table
 global $wpdb;
-$table_name = $wpdb->prefix .'woocommerce_scanpay_seq';
-$wpdb->query("DROP TABLE IF EXISTS {$table_name}");
+$seq_table = $wpdb->prefix . 'woocommerce_scanpay_seq';
+$sub_table = $wpdb->prefix . 'woocommerce_scanpay_queuedcharges';
+
+// delete database table
+$wpdb->query("DROP TABLE IF EXISTS {$seq_table}");
+$wpdb->query("DROP TABLE IF EXISTS {$sub_table}");
 
 // Delete plugin settings
 delete_option('woocommerce_scanpay_settings');
