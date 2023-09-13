0c155078addaf87569d70d5346759982dd55d23a
Ulrik Moe <ulrik@scanpay.dk>
2023-09-13 14:46:59 +0200

    Remove sync file

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,45 @@ const WooCommerceDependencyExtractionWebpackPlugin = require('@woocommerce/depen
 const path = require('path');
 
 const wcDepMap = {
-	'@woocommerce/blocks-registry': ['wc', 'wcBlocksRegistry'],
-	'@woocommerce/settings'       : ['wc', 'wcSettings']
+    '@woocommerce/blocks-registry': ['wc', 'wcBlocksRegistry'],
+    '@woocommerce/settings'       : ['wc', 'wcSettings']
 };
 
 const wcHandleMap = {
-	'@woocommerce/blocks-registry': 'wc-blocks-registry',
-	'@woocommerce/settings'       : 'wc-settings'
+    '@woocommerce/blocks-registry': 'wc-blocks-registry',
+    '@woocommerce/settings'       : 'wc-settings'
 };
 
 const requestToExternal = (request) => {
-	if (wcDepMap[request]) {
-		return wcDepMap[request];
-	}
+    if (wcDepMap[request]) {
+        return wcDepMap[request];
+    }
 };
 
 const requestToHandle = (request) => {
-	if (wcHandleMap[request]) {
-		return wcHandleMap[request];
-	}
+    if (wcHandleMap[request]) {
+        return wcHandleMap[request];
+    }
 };
 
 // Export configuration.
 module.exports = {
-	...defaultConfig,
-	entry: {
-		'blocks': '/public/js/blocks.js',
-	},
-	output: {
-		path: path.resolve( __dirname, 'public/min' ),
-		filename: '[name].js',
-	},
-	plugins: [
-		...defaultConfig.plugins.filter(
-			(plugin) =>
-				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
-		),
-		new WooCommerceDependencyExtractionWebpackPlugin({
-			requestToExternal,
-			requestToHandle
-		})
-	]
+    ...defaultConfig,
+    entry: {
+        'blocks': '/public/js/blocks.js',
+    },
+    output: {
+        path: path.resolve( __dirname, 'public/min' ),
+        filename: '[name].js',
+    },
+    plugins: [
+        ...defaultConfig.plugins.filter(
+            (plugin) =>
+                plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
+        ),
+        new WooCommerceDependencyExtractionWebpackPlugin({
+            requestToExternal,
+            requestToHandle
+        })
+    ]
 };
