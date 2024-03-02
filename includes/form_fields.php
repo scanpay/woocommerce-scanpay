e9103c924c2b92e4b4a7f9eb661361653cad8c6c
Ulrik Moe <ulrik.moe@gmail.com>
2024-03-02 14:25:46 +0000

    Add file with form_fields

---
+++ includes/form_fields.php
@@ -0,0 +1,83 @@
+<?php
+
+defined( 'ABSPATH' ) || exit();
+
+return [
+	'enabled'              => [
+		'title'   => 'Enable',
+		'type'    => 'checkbox',
+		'label'   => 'Enable Scanpay in the checkout.',
+		'default' => 'no',
+	],
+	'apikey'               => [
+		'title'             => 'API key',
+		'type'              => 'text',
+		'custom_attributes' => [ 'autocomplete' => 'off' ],
+		'default'           => '',
+	],
+	'title'                => [
+		'title'       => 'Title',
+		'type'        => 'text',
+		'description' => 'A title for the payment method. This is displayed on the checkout page.',
+		'desc_tip'    => true,
+		'default'     => 'Betal med kort',
+	],
+	'description'          => [
+		'title'       => 'Description',
+		'type'        => 'text',
+		'description' => 'A description of the payment method. This is displayed on the checkout page.',
+		'desc_tip'    => true,
+		'default'     => 'Betal med betalingskort via Scanpay.',
+	],
+	'card_icons'           => [
+		'title'       => 'Card icons',
+		'type'        => 'multiselect',
+		'description' => 'Choose which card icons to display on the checkout page.',
+		'options'     => [
+			'dankort'    => 'Dankort',
+			'visa'       => 'Visa',
+			'mastercard' => 'Mastercard',
+			'maestro'    => 'Maestro',
+			'amex'       => 'American Express',
+			'diners'     => 'Diners',
+			'discover'   => 'Discover',
+			'unionpay'   => 'UnionPay',
+			'jcb'        => 'JCB',
+		],
+		'class'       => 'wc-enhanced-select',
+		'desc_tip'    => true,
+		'default'     => [ 'visa', 'mastercard' ],
+	],
+	'stylesheet'           => [
+		'title'   => 'Stylesheet',
+		'type'    => 'checkbox',
+		'label'   => 'Use default checkout stylesheet (CSS).',
+		'default' => 'yes',
+	],
+	'capture_on_complete'  => [
+		'title'       => 'Auto-Capture',
+		'type'        => 'checkbox',
+		'label'       => 'Capture when order status is changed to "completed".',
+		'description' => 'Automatically capture the payment when the order status changes to "completed".',
+		'desc_tip'    => true,
+		'default'     => 'yes',
+	],
+	'wc_complete_virtual'  => [
+		'title'   => 'Auto-Complete',
+		'type'    => 'checkbox',
+		'label'   => 'Auto-complete virtual orders.',
+		'default' => 'no',
+	],
+	'wcs_complete_initial' => [
+		'title'   => '&#10240;',
+		'type'    => 'checkbox',
+		'label'   => 'Auto-complete new subscribers <i>(Subscriptions only)</i>.',
+		'default' => 'no',
+	],
+	'wcs_complete_renewal' => [
+		'title'   => '&#10240;',
+		'type'    => 'checkbox',
+		'label'   => 'Auto-complete renewal orders <i>(Subscriptions only)</i>.',
+		'default' => 'no',
+	],
+];
