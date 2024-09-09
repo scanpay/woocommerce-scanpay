44222610caa456697b3e42fa930428e8805e64c6
Ulrik Moe <ulrik.moe@gmail.com>
2024-09-09 13:56:55 +0200

    Convert blocks.js into TypeScript and rename it to checkout

---
+++ src/public/js/checkout/index.d.ts
@@ -0,0 +1,37 @@
+/**
+ *	External definitions for WooCommerce Blocks.
+ */
+import * as wpElement from '@wordpress/element';
+
+declare global {
+	interface Window {
+		wp: {
+			element: typeof wpElement;
+		};
+		wc: {
+			wcBlocksRegistry: {
+				//defined in woocommerce/plugins/woocommerce-blocks/assets/js/blocks-registry/index.js
+				registerPaymentMethod: any;
+			};
+			wcSettings: {
+				getSetting: ( key: string ) => any;
+			};
+		};
+	}
+}
+
+/**
+ * Data structure for the payment methods. Defined in class-wc-scanpay-blocks-support.php.
+ */
+
+interface WooPaymentMethodData {
+	methods: {
+		[ key: string ]: {
+			title: string;
+			description: string;
+			icons: string[];
+			supports: string[];
+		};
+	};
+	url: string; // URL to the plugin's assets
+}
