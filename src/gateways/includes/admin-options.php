dcd7ca14613d40c48e85e27544358a75da6c880e
Ulrik Moe <ulrik.moe@gmail.com>
2026-05-04 15:35:51 +0200

    Refactor log handling and add logs link to Scanpay admin options

--- src/gateways/includes/admin-options.php
+++ src/gateways/includes/admin-options.php
@@ -54,27 +54,22 @@ if ( ! $shopid ) {
 }
 
 // Check for unread scanpay logs
-$files = WC_Log_Handler_File::get_log_files();
+$files        = WC_Log_Handler_File::get_log_files();
 $scanpay_logs = array_filter(
-    array_keys( $files ),
-    fn ( $file ) => str_starts_with( $file, 'wc-scanpay' )
+	array_keys( $files ),
+	fn ( $file ) => str_starts_with( $file, 'wc-scanpay' )
 );
-if ( count ( $scanpay_logs ) > 0 ) {
-	// Construct the log file name.
-	$logs_url    = add_query_arg(
-		[
-			'page'     => 'wc-status',
-			'tab'      => 'logs',
-			'log_file' => basename( WC_Log_Handler_File::get_log_file_path( 'wc-scanpay' ) ),
-			'source'   => 'wc-scanpay',
-		],
-		admin_url( 'admin.php' )
-	);
-	scanpay_admin_notice( "You have unread logs", "warning" );
-}
 
-
-
+// Construct the log file name.
+$logs_url = add_query_arg(
+	[
+		'page'     => 'wc-status',
+		'tab'      => 'logs',
+		'log_file' => basename( WC_Log_Handler_File::get_log_file_path( 'wc-scanpay' ) ),
+		'source'   => 'wc-scanpay',
+	],
+	admin_url( 'admin.php' )
+);
 
 ?>
 
@@ -96,10 +91,11 @@ $tabs = [
 	'scanpay_applepay'  => 'Apple Pay',
 ];
 ?>
+
 <div class="wcsp-nav wcsp-nav-<?php echo $gateway->id; ?>" aria-label="Scanpay menu">
 	<?php foreach ( $tabs as $id => $label ) : ?>
 		<?php
-		$url     = add_query_arg(
+		$url = add_query_arg(
 			[
 				'page'    => 'wc-settings',
 				'tab'     => 'checkout',
@@ -107,16 +103,17 @@ $tabs = [
 			],
 			admin_url( 'admin.php' )
 		);
+
 		$classes = 'wcsp-nav-tab';
 		if ( $gateway->id === $id ) {
 			$classes .= ' wcsp-nav-tab-active';
 		}
 		?>
-		<a class="<?php echo esc_attr( $classes ); ?>"
-			href="<?php echo esc_url( $url ); ?>">
+		<a class="<?php echo esc_attr( $classes ); ?>" href="<?php echo esc_url( $url ); ?>">
 			<?php echo esc_html( $label ); ?>
 		</a>
 	<?php endforeach; ?>
+	<a class="wcsp-nav-logs" href="<?php echo esc_url( $logs_url ); ?>">Logs</a>
 </div>
 
 <table class="form-table wcsp-set-<?php echo esc_attr( $gateway->id ); ?>">
